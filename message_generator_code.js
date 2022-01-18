a7af5597a6210be86cd41cc502b254f265610217
Javier Retamal <javier.retamal.fontannaz@gmail.com>
2022-01-18 11:40:32 -0300

    Add lab items and tasks to generate random message

--- message_generator_code.js
+++ message_generator_code.js
@@ -1,3 +1,9 @@
 const randomMessage = (num) => {
     return Math.floor(Math.random() * num);
 };
+
+const labThings = {
+    labObjects: ['Thermocycler', 'Centrifuge', 'Electrophoresis chamber', 'Nanoquant'],
+    labTasks: ['PCR', 'Westernblot', 'RNA extraction', 'DNA extraction', 'Real-Time PCR'],
+    labConsumables: ['Eppendorf tubes', 'Micropippete tips', 'Cell culture medium', 'Nitrile gloves']
+};
