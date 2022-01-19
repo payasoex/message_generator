df4c3fa3a761a1dff6bb8176d0f65d6c2a10e8dd
Javier Retamal <javier.retamal.fontannaz@gmail.com>
2022-01-19 10:59:48 -0300

    add a iterate action above labthings to create a random message

--- message_generator_code.js
+++ message_generator_code.js
@@ -1,11 +1,34 @@
+// to generate random number
 const randomMessage = (num) => {
     return Math.floor(Math.random() * num);
 };
 
+// lab stuff to use in our sentence
 const labThings = {
-    labObjects: ['Thermocycler', 'Centrifuge', 'Electrophoresis chamber', 'Nanoquant'],
+    labInstruments: ['Thermocycler', 'Centrifuge', 'Electrophoresis chamber', 'Nanoquant'],
     labTasks: ['PCR', 'Westernblot', 'RNA extraction', 'DNA extraction', 'Real-Time PCR'],
     labConsumables: ['Eppendorf tubes', 'Micropippete tips', 'Cell culture medium', 'Nitrile gloves']
 };
 
-let labFrase = [];
+// to store our sentence in an array
+let labSentence = [];
+
+// iterate over the object on labThings
+for (let thing in labThings) {
+    let optionIdx = randomMessage(labThings[thing].length);
+
+// use object's properties to create a message
+    switch (thing) {
+        case 'labObjects':
+            labSentence.push(`Today, you will use the "${labInstruments[thing][optionIdx]}" for your experiment.`);
+            break;
+        case 'labTasks':
+            labSentence.push(`Todoay is a great day for to do "${labTasks[thing][optionIdx]}".`);
+            break;
+        case 'labConsumables':
+            labSentence.push(`Oh unfortunately the "${labConsumables[thing][optionIdx]}" are over, you will have to notify the laboratory manager.`);
+            break;
+        default:
+            labSentence.push('There is no enough info.');
+    }    
+}
